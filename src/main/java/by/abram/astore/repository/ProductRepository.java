79a819399461956951eb395f40e47ce1f63842b6
iamFOOSA <abramchukdanila@gmail.com>
2026-02-24 22:13:04 +0300

    commit 1_5

--- src/main/java/by/abram/astore/repository/ProductRepository.java
+++ src/main/java/by/abram/astore/repository/ProductRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategory(String category);
-    List<Product> findByNameContaining(String name);
 }
